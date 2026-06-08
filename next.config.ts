f6d9da1cd9531fd91a72c3e041e3b478f73f0bca
Wawan Setiawan <wsnsmd@gmail.com>
2026-06-08 23:36:01 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'bpbd.kaltimprov.go.id' },
       {
